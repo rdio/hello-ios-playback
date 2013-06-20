e8ab054e0b48039c3745843a0cb49775224cdcb2
R. Kevin Nelson <rkn@rd.io>
2013-06-20 11:46:23 -0700

    Update style on HelloAppDelegate.h

--- Classes/HelloAppDelegate.h
+++ Classes/HelloAppDelegate.h
@@ -10,14 +10,9 @@
 #import <Rdio/Rdio.h>
 #import "HelloViewController.h"
 
-@interface HelloAppDelegate : NSObject <UIApplicationDelegate> {
-    UIWindow *window;
-    HelloViewController *viewController;
-	
-    Rdio *rdio;
-}
+@interface HelloAppDelegate : NSObject <UIApplicationDelegate>
 
-+(Rdio *)rdioInstance;
++ (Rdio *)rdioInstance;
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet HelloViewController *viewController;
